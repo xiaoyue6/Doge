d26065bf3907fd494f582dfcb9de5e566cb9f1d7
xiaoyue6 <xiaoyue65@outlook.com>
2015-02-28 16:26:43 +0800

    呵呵

--- src/com/cuit/DogeMusicDataService/dao/ILoginDao.java
+++ src/com/cuit/DogeMusicDataService/dao/ILoginDao.java
@@ -4,6 +4,9 @@ import com.cuit.DogeMusicDataService.model.vo.LoginVo;
 import com.cuit.DogeMusicDataService.model.vo.UserFriendVo;
 import com.cuit.DogeMusicDataService.model.vo.UserMsgVo;
 
+/**
+ * 登录相关接口
+ * */
 public interface ILoginDao {
 	/**
 	 * 用户登录
